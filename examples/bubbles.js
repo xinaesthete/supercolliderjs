6b161fc025a59f084595ba51eef76a5ce16a1201
Peter Todd <ptr.tdd@gmail.com>
2025-02-17 07:52:19 +0000

    update bubbles to use async, add delayTime synth parameter

--- examples/bubbles.js
+++ examples/bubbles.js
@@ -1,13 +1,14 @@
 const sc = require("supercolliderjs");
 
-sc.server.boot().then(server => {
-  const def = server.synthDef(
+sc.server.boot().then(async (server) => {
+  // should be using await here?
+  const def = await server.synthDef(
     "bubbles",
     `
-      SynthDef("bubbles", { arg out=0, wobble=0.4, innerWobble=8, releaseTime=4;
+      SynthDef("bubbles", { arg out=0, wobble=0.4, innerWobble=8, releaseTime=4, delayTime=0.2;
         var f, zout;
         f = LFSaw.kr(wobble, 0, 24, LFSaw.kr([innerWobble, innerWobble / 1.106], 0, 3, 80)).midicps;
-        zout = CombN.ar(SinOsc.ar(f, 0, 0.04), 0.2, 0.2, 4);  // echoing sine wave
+        zout = CombN.ar(SinOsc.ar(f, 0, 0.04), delayTime, 0.2, 4);  // echoing sine wave
         zout = zout * EnvGen.kr(Env.linen(releaseTime: releaseTime), doneAction: 2);
         Out.ar(out, zout);
       });
@@ -19,6 +20,7 @@ sc.server.boot().then(server => {
       wobble: Math.random() * 10,
       innerWobble: Math.random() * 16,
       releaseTime: Math.random() * 4 + 2,
+      delayTime: Math.random() * 0.2 + 0.1,
     });
   }, 4000);
 });
