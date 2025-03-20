8be4264d07fbeeaba7fa35998fb532a58ae36b17
Peter Todd <ptr.tdd@gmail.com>
2025-03-20 07:59:56 +0000

    comment

--- examples/bubbles.cjs
+++ examples/bubbles.cjs
@@ -3,6 +3,7 @@ const sc = require("supercolliderjs");
 sc.server.boot({
   numInputBusChannels: '0'
 }).then(server => {
+  // this has the same problem as bubbles.mjs - server.synthDef is undefined
   const def = server.synthDef(
     "bubbles",
     `
